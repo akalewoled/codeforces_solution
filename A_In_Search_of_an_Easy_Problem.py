7ed1a294db0c818fa4f50f357e42dd8de4f99a30
akalewoled <fikerbdu@gmail.com>
2024-02-14 18:42:11 +0200

    first question

---
+++ A_In_Search_of_an_Easy_Problem.py
@@ -0,0 +1,14 @@
+""""
+fiker at A@SV
+"""
+a=int(input())
+inputed=list(input().split())
+final="EASY"
+for i in inputed:
+    if i=='1':
+        final="HARD"
+        break
+print(final)
+    
+
+
