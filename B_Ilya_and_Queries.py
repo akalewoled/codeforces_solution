0bab18b0b67a1c1dd9bcfd30445a6cca81e61093
akalewoled <fikerbdu@gmail.com>
2024-04-01 19:36:02 +0200

    contest 8 analysis

--- B_Ilya_and_Queries.py
+++ B_Ilya_and_Queries.py
@@ -1,12 +1,13 @@
 string=input() 
 n=int(input())
-presum=[1] # the list to hold the repitition of the valid indexes'
+presum=[1] # the list to hold the repitition of the valid indexes
 
 c=1 # temporary counter
 for i in range(1,len(string)):
     if string[i]==string[i-1]:
         c+=1
     presum.append(c)
+print(presum)
 for i in range(n):
     l,r=map(int,input().split())
     print(presum[r-1]-presum[l-1])
