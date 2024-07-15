a7020aa048e43ad01a8629ef49d5e33ebda43754
akalewoled <fikerbdu@gmail.com>
2024-07-15 13:40:40 +0200

    contest before contest number 22

--- C_Kefa_and_Park.py
+++ C_Kefa_and_Park.py
@@ -1,7 +1,9 @@
 n,m=map(int,input().split())
 
 cat=list(map(int,input().split()))
-d = {i:[] for i in range(1,n+1)}
+
+d = {i:[] for i in range(1,n+1)}#our graph of direction 
+
 for i in range(n-1):
     a,b=map(int,input().split())
     d[a].append(b)
@@ -16,7 +18,7 @@ while q:
 
     c=cat[k-1]*(c+1) #multiply the 
 
-    if c>m:# if we have collected the amount of the values he can bear jump
+    if c>m:# if we have collected the amount of the cats he can bear 
         continue
 
     if len(d[k])==1 and k!=1:# we have reached the node which have one nebhiour and it is not the first node
